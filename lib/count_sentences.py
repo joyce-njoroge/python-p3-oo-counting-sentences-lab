1e121fc8a031e1f843fd6046ddaacd35e23a9bfc
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-22 16:06:52 +0300

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, string=123):
+    self.string = string
+    if not isinstance(string, str):
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.string.endswith('.')
+  
+  def is_question(self):
+    return self.string.endswith('?')
+  
+  def is_exclamation(self):
+    return self.string.endswith('!')
+  
+  def count_sentences(self):
+    import re
+    pattern = r'\b\w[^\.\?\!]*[\.\?\!]'
+    sentence_list = re.findall(pattern, str(self.string))
+    return len(sentence_list)
+  
+string = MyString("This, well, is a sentence. This too!! And so is this, I think? Woo...")
